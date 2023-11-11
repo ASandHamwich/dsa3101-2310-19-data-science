3fe890db8bcd09e35cd5aaeb79d81e39666d9b58
zhiyinnn <141896780+zhiyinnn@users.noreply.github.com>
2023-11-12 02:03:23 +0800

    Added logo to the header

--- frontend/app.py
+++ frontend/app.py
@@ -15,7 +15,7 @@ header = html.Div(
         dcc.Link(
             id = "header",
             children = [
-                html.Img(src = 'assets\logo.png', className = "logo")
+                html.Img(src = '/assets/logo.png', className = "logo")
             ], 
             href = '/home'
         )
