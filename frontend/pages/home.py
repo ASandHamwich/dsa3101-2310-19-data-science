898992d28a60c85a50b06c78e61a9cb0693f874a
zhiyinnn <141896780+zhiyinnn@users.noreply.github.com>
2023-11-13 15:46:13 +0800

    Updated aesthetic of compare button

--- frontend/pages/home.py
+++ frontend/pages/home.py
@@ -38,9 +38,13 @@ layout = html.Div(
         ),
 
         html.Div(
-            html.Button('Compare', id = 'comp'),
-            style = {'display':'flex', 'margin': '25px', 'justifyContent': 'center'}
-        )
+            html.Button('Compare', id = 'comp', style = {
+                'background-color':'#F9CA84', 'border-color':'#F9CA84', 'font-size':'25px', 'border-radius':'10px', 'margin': '20px'
+            }),
+            style = {'display':'flex', 'margin': '20px', 'justifyContent': 'center'}
+        ),
+
+        html.Br()
     ],
     style = {'background-color':'white'}
 )
@@ -76,7 +80,7 @@ def buttonPress(nus1_clicks, nus2_clicks, ntu1_clicks, smu1_clicks, comp, checkb
         if 'comp' in changed_id:
 
             checkbox = []
-            cb_dict = {'nus-dsa':checkbox_nus1, 'nus-dse':checkbox_nus2, 'ntu-dsa':checkbox_ntu1, 'smu-dsa':checkbox_smu1}
+            cb_dict = {'nus1':checkbox_nus1, 'nus2':checkbox_nus2, 'ntu1':checkbox_ntu1, 'smu1':checkbox_smu1}
             for key in cb_dict:
                 if  cb_dict[key] == None:
                     continue
