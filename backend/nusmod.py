b43f926b1f24935cc77fb7232842dbb039a2f16d
AshtonLow <142159295+AshtonLow@users.noreply.github.com>
2023-11-13 16:24:32 +0800

    added minor comments for nusmod.py

--- backend/nusmod.py
+++ backend/nusmod.py
@@ -1,33 +1,35 @@
 import requests
 from bs4 import BeautifulSoup, NavigableString
 import pandas as pd
-from selenium import webdriver
+from selenium import webdriver #pip install selenium
 import time
 import csv
 NusDsaDescriptionList = []
 NusDsaTitleList = []
 NusDseDescriptionList = []
 NusDseTitleList = []
+#list of modules obtained from nus dsa/dse requirements from official nus website
 NusDsaMainMods = ['DSA1101','CS1010S','CS2040','DSA2101','DSA2102','MA2001','MA2002','MA2311','ST2131','ST2132','CS3244','DSA3101','DSA3102','ST3131','DSE4211','DSE4212','DSA4288']
-NusDsaORSpecMods = ['MA3236','MA3252','MA3227','MA3238','ST3236','MA4230','MA4251','ST4238','MA4260','MA4268','MA4270','DSA4288M']
-NusDsaSMSpecMods = ['ST3232','ST3239','ST3247','ST3248','ST4231','ST4234','ST4248','ST4250','ST4253','DSA4288S']
+NusDsaORSpecMods = ['MA3236','MA3252','MA3227','MA3238','ST3236','MA4230','MA4251','ST4238','MA4260','MA4268','MA4270','DSA4288M'] #Operation Research Specialization
+NusDsaSMSpecMods = ['ST3232','ST3239','ST3247','ST3248','ST4231','ST4234','ST4248','ST4250','ST4253','DSA4288S'] #Statistical Methodology Specialization
 NusDsaModsCode = NusDsaMainMods+NusDsaORSpecMods+NusDsaSMSpecMods
 NusDseModsCode = ['DSE1101','CS1010S','EC1101E','CS2040','DSA2101','DSA2102','EC2101','EC2102','MA2001','MA2002','MA2311','MA2104','ST2131','MA2116','ST2132','DSA3102','DSE3101','EC3101','EC3102','EC3304','ST3131','DSE4101','EC4305','DSA4264','DSA4265','DSE4201','DSE4211','QF4211','DSE4212','QF4212','DSE4231','EC4308']
 baseurl = "https://nusmods.com/courses/"
+#list of url for each module in DSA and DSE
 NusDsaModsURL = [baseurl+x for x in NusDsaModsCode]
 NusDseModsURL = [baseurl+x for x in NusDseModsCode]
 for x in range(len(NusDsaModsCode)):
     url = NusDsaModsURL[x]
     driver = webdriver.Chrome()
-    driver.get(url)
-    time.sleep(10)
+    driver.get(url) 
+    time.sleep(10) 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.findAll('div',attrs={"class":"col-sm-8"})
     ls = []
     for i in table:
-        ls.append(i.find('p').text)
-    table = soup.find('h1',attrs={"class":"kbFQ8zbG"}).contents[-1]
+        ls.append(i.find('p').text) #getting the module description
+    table = soup.find('h1',attrs={"class":"kbFQ8zbG"}).contents[-1] #getting the module title
     NusDsaDescriptionList.append(''.join(ls))
     NusDsaTitleList.append(str(table))
 for x in range(len(NusDseModsCode)):
@@ -40,8 +42,8 @@ for x in range(len(NusDseModsCode)):
     table = soup.findAll('div',attrs={"class":"col-sm-8"})
     ls = []
     for i in table:
-        ls.append(i.find('p').text)
-    table = soup.find('h1',attrs={"class":"kbFQ8zbG"}).contents[-1]
+        ls.append(i.find('p').text) #getting the module description
+    table = soup.find('h1',attrs={"class":"kbFQ8zbG"}).contents[-1] #getting the module title
     NusDseDescriptionList.append(''.join(ls))
     NusDseTitleList.append(str(table))
 Dsadata = {'NUS Module Code': NusDsaModsCode, 'NUS Module Title': NusDsaTitleList, 'NUS Module Description': NusDsaDescriptionList}
