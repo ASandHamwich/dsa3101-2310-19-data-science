fea9c414f9cbea7905304b46196991b535c16505
xinensgit <142471018+xinensgit@users.noreply.github.com>
2023-11-13 23:40:50 +0800

    beautifying the search box

--- frontend/app.py
+++ frontend/app.py
@@ -3,40 +3,63 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import requests
-
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True)
 
-#Top header 
+# Top header
 header = html.Div(
-    className = 'header',
-    children = [
+    className='header',
+    children=[
         dcc.Link(
-            id = "header",
-            children = [
-                html.Img(src = '/assets/logo.png', className = "logo")
-            ], 
-            href = '/home'
+            id="header",
+            children=[
+                html.Img(src='/assets/logo.png', className="logo")
+            ],
+            href='/home'
         )
     ]
 )
 
-
 search_bar = html.Div(
-    children = [
-        dcc.Location(id = 'search_nav'),
-        dcc.Input(
-            id = 'search', 
-            type = 'text',
-            placeholder = 'Search Concepts, Modules here...', 
-            size = '70', 
-            style = {'margin': '50px auto', 'height': '30px', 'font-size': '20px'},
+    children=[
+        dcc.Location(id='search_nav'),
+        html.Div(
+            children=[
+                html.Img(src='/assets/glass_logo.png', className='magnifying-glass-icon', style={
+                    'width': '40px',
+                    'height': '30px',
+                    'margin-right': '0px',
+                    'margin-top': '0px',
+                    'background-color': '#F9CA84',
+                    'padding': '5px',
+                    'border-top-left-radius': '15px',
+                    'border-bottom-left-radius': '15px',
+                    'border-top-right-radius': '0px',
+                    'border-bottom-right-radius': '0px',
+                }),
+                dcc.Input(
+                    id='search',
+                    type='text',
+                    placeholder='Search Universities, Modules here...',
+                    size='70',
+                    style={
+                        'height': '30px',
+                        'font-size': '20px',
+                        'border': '4px solid #F9CA84',
+                        'border-radius': '15px',
+                        'padding-left': '10px',
+                        'border-top-left-radius': '0px',  
+                        'border-bottom-left-radius': '0px',
+                        'border-top-right-radius': '15px',
+                        'border-bottom-right-radius': '15px', 
+                    },
+                ),
+            ],
+            style={'margin': '50px auto', 'display': 'flex', 'justifyContent': 'center'}
         ),
-    ],
-    style = {'display': 'flex', 'justifyContent': 'center'}
+    ]
 )
-
 
 app.layout = html.Div(
     [
@@ -46,16 +69,11 @@ app.layout = html.Div(
     ]
 )
 
-
-@callback(
+@app.callback(
     Output("search_nav", "pathname"),
-    [
-        Input("search", "value"), 
-        Input("search", "n_submit")
-    ],
-    prevent_initial_call = True
+    [Input("search", "value"), Input("search", "n_submit")],
+    prevent_initial_call=True
 )
-
 def searchFunction(query, submit):
     if submit == 1:
         return f'/search={query}'
