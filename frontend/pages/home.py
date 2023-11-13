66b8590dde7b0423b43b5b7f2b492522aaddea53
xinensgit <142471018+xinensgit@users.noreply.github.com>
2023-11-13 19:44:43 +0800

    setted up the button logos and color

--- frontend/pages/home.py
+++ frontend/pages/home.py
@@ -8,36 +8,33 @@ dash.register_page(__name__, path='/home')
 def course_layout(uni_code):
     if uni_code == 'nus-dsa':
         name = "Data Science & Analytics"
-        school = "National University of Singapore"
-        desc = "The four-year direct Honours programme is designed to prepare graduates who are ready to acquire, manage and explore data that will inspire change around the world. Students will read courses in Mathematics, Statistics and Computer Science, and be exposed to the interplay between these three key areas in the practice of data science."
+        school = "NUS"
         img_path = "/assets/nus_pic.png"
 
     if uni_code == 'nus-dse':
         name = "Data Science & Economics"
-        school = "National University of Singapore"
-        desc = "The Data Science and Economics (DSE) cross-disciplinary programme (XDP) aims to produce students who have strong foundation knowledge in data science and economics as well as hands-on experience with empirical analysis of economic data, to analyse and interpret the local and global impact of data on individuals, organisation, society and the global economic ecosystem. The DSE curriculum incorporates inter-disciplinary learning from data science and economics, with foundations in computer science, mathematics and statistics. In addition to higher-level courses that integrate knowledge and concepts from lower-level core foundational courses, students also read courses related to the application of data science and analytics to the financial market, labour market, and other applied economic issues in education, health, housing and industrial organisation."
+        school = "NUS"
         img_path = "/assets/nus_pic.png"
 
     if uni_code == 'ntu-dsa':
         name = "Data Science and Artificial Intelligence"
-        school = "Nanyang Technological University"
-        desc = "An undergraduate degree programme in Data Science and Artificial Intelligence, based on rigorous training in the synergistic fields of statistics and computer science. The programme, which is run jointly by the School of Computer Science and Engineering and the School of Physical and Mathematical Sciences, targets students who have the vision of using data science and artificial intelligence (AI) to find innovative solutions to society’s pressing challenges. The curriculum provides students with opportunities to solve real-life problems in different applications domains ranging from science and technology, healthcare, business and finance, environmental sustainability, and more."
+        school = "NTU"
         img_path = "/assets/ntu_pic.jpg"
 
     if uni_code == 'smu-dsa':
         name = "Data Science & Analytics (2nd Major)"
-        school = "Singapore Management University"
-        desc = "The DSA second major focuses on applications of statistical modelling, machine learning algorithms, computing and information technology as well as simulation and predictive approaches to solve real-world problems encountered in all private and public institutions. The curriculum of the second major adopts a hands-on pedagogy in mathematics, statistics and computer science, emphasizing practical applications related to economics, social sciences, finance, risk management, business, insurance, and more."
+        school = "SMU"
         img_path = '/assets/smu_pic.jpg'
 
-    return name, school, desc, img_path
+    return name, school, img_path
 
 def buttonFormat(name, id, image_url, img_style=None):
     return html.Div(
         children=[
             html.Img(src=image_url, className='checkbox-image', style=img_style),
             dbc.Checkbox(id=f'checkbox_{id}', className='course--add', style={'margin-top': '10px'}),
-            html.Button(name, id=id, className='course--link', style={'margin-top': '0px'})
+            html.Button(name, id=id, className='course--link', style={'margin-top': '0px', 'color': '#0A1173'})
+            #html.Button(name, id=id, className='course--link', style={'margin-top': '0px'})
         ],
         className='course--rect'
     )
@@ -53,13 +50,13 @@ layout = html.Div(
             children=[
                 dcc.Location(id='url'),
                 buttonFormat('NUS: Data Science and Analytics', 'nus-dsa', '/assets/nus_logo.png', 
-                    img_style={'width': '150px', 'height': '80px', 'margin-left': '30px', 'margin-top': '15px'}),
+                    img_style={'width': '150px', 'height': '80px', 'margin-left': '30px', 'margin-top': '20px'}),
                 buttonFormat('NUS: Data Science and Economics', 'nus-dse', '/assets/nus_logo.png', 
-                    img_style={'width': '150px', 'height': '80px', 'margin-left': '30px', 'margin-top': '15px'}),
+                    img_style={'width': '150px', 'height': '80px', 'margin-left': '30px', 'margin-top': '20px'}),
                 buttonFormat('NTU: Data Science and Artificial Intelligence', 'ntu-dsa', '/assets/ntu_logo.png', 
-                    img_style={'width': '250px', 'height': '250px', 'margin-left': '30px', 'margin-top': '0px'}),
-                buttonFormat('SMU: Data Science and Analytics (2nd Major)', 'smu-dsa', '/assets/smu-logo.png', 
-                    img_style={'width': '120px', 'height': '80px', 'margin-left': '30px', 'margin-top': '15px'})
+                    img_style={'width': '170px', 'height': '75px', 'margin-left': '30px', 'margin-top': '20px'}),
+                buttonFormat('SMU: Data Science and Analytics (2nd Major)', 'smu-dsa', '/assets/smu_logo.png', 
+                    img_style={'width': '160px', 'height': '85px', 'margin-left': '30px', 'margin-top': '10px'})
             ],
             className='course'
         ),
@@ -67,7 +64,7 @@ layout = html.Div(
         html.Div(
             html.Button('Compare', id='comp', style={
                 'background-color': '#F9CA84', 'border-color': '#F9CA84', 'font-size': '25px',
-                'border-radius': '10px', 'margin': '20px'
+                'border-radius': '10px', 'margin': '20px', 'color': '#FFFFF'
             }),
             style={'display': 'flex', 'margin': '20px', 'justifyContent': 'center'}
         ),
