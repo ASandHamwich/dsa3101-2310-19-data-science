8865086ebc8233c78a484a47797b2f3441301c79
Peng Kai <141643791+pkheng5@users.noreply.github.com>
2023-11-16 23:59:59 +0800

    Update course.py

--- frontend/pages/course.py
+++ frontend/pages/course.py
@@ -206,7 +206,7 @@ def course_links(uni_code):
 
 
 def fetch_data(uni_code):
-    url = f"http://localhost:5001/{uni_code}" #prereq information 
+    url = f"http://backend-1:5001/{uni_code}" #prereq information 
     uni = uni_code[0:3]
     full_module_data = eval(str(requests.get(url).text))[uni]["modules"] # returns a list of dictionaries for all mods
     # Note that some prereqs are not part of the core curriculum; those will be left out of the final graph.
@@ -219,7 +219,7 @@ def fetch_data(uni_code):
     return mod_list, full_module_data
 
 def fetch_all():
-    url = 'http://localhost:5001/nus-ntu-smu/all-modules/'
+    url = 'http://backend-1:5001/nus-ntu-smu/all-modules/'
     return eval(str(requests.get(url).text))
 
 
