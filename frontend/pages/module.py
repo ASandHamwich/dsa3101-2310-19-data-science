9234a378b349cb5af36907f93a7c55ce8077cad2
ASandHamwich <e0726350@u.nus.edu>
2023-11-17 01:40:48 +0800

    Added descriptions for hover elements in module key concepts.

--- frontend/pages/module.py
+++ frontend/pages/module.py
@@ -25,24 +25,31 @@ def conceptsBar(uni_code, mod_code):
     #Load the relevant data concepts
     data_dict = fetch_data(uni_code, mod_code)
     concepts = data_dict["key_concepts"].upper().split(", ")
+    glossary_list = [fetch_glossary()[x.lower()] for x in concepts]
 
     #Create dataframe 
     data = {
         "concept": concepts,
+        "explanation": glossary_list,
         "val": [1 for i in range(len(concepts))],
         "x": [1 for i in range(len(concepts))]
     }
     df = pd.DataFrame(data)
 
     fig = px.bar(
-        df, x = "x", y = 'val', 
+        df, x = "x", y = 'val', custom_data = "explanation",
         color = 'concept', text = 'concept', orientation="h")
+
+
     
     fig.update_traces(textfont_size = 20, textposition="inside", insidetextanchor="middle", showlegend=False, width = 0.5)
     fig.update_yaxes(showgrid = False, visible = False)
     fig.update_xaxes(showgrid = False, visible = False)
     fig.update_layout(plot_bgcolor = 'rgba(0, 0, 0, 0)')
     fig.update_layout(margin = dict(l = 0.1, r = 0.1, t = 0.1, b = 0.1, pad = 0))
+    print("user_defined hovertemplate:", fig.data[0].hovertemplate)
+    fig.update_traces(hovertemplate='%{customdata}')
+    #print("user_defined hovertemplate:", fig.data[0].hovertemplate)
 
 
     return html.Div(
